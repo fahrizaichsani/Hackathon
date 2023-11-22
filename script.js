32be98d5f7219f1c51a7a7f67e35ceefc01909f6
fahrizaichsani <fahrizaichsanirafif@gmail.com>
2023-11-22 16:47:47 +0700

    Wele

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const gameContainer = document.body;
 const catcher = document.getElementById("catcher");
-
+const gifkucing = document.getElementById('gifkucing')
 function moveCatcher() {
     document.addEventListener("mousemove", e => {
         let mousePosition = e.clientX;
         catcher.style.left = `${mousePosition - catcher.clientWidth/2}px`
+
+        gifkucing.style.left = `${mousePosition - gifkucing.clientWidth/2}px`
     })
 }
 moveCatcher();
